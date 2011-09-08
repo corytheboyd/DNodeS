d9afbd322a33d2bc6ecfec411a2a5c0d543d427d
Cory Boyd <corytheboyd@gmail.com>
2011-09-07 17:31:15 -0700

    updated read me, removed old schema files

--- sql/ns_schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE ns (
-  id INTEGER not null AUTO_INCREMENT not null,
-  zone varchar(255) not null,
-  server varchar(255) not null,
-  owner varchar(255) not null,
-  PRIMARY KEY (id),
-  FOREIGN KEY (zone) REFERENCES zones(domain) ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- sql/txt_schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE txt (
-  id INTEGER not null AUTO_INCREMENT not null,
-  zone varchar(255) not null,
-  text varchar(255) not null,
-  owner varchar(255) not null,
-  PRIMARY KEY (id),
-  FOREIGN KEY (zone) REFERENCES zones(domain) ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
